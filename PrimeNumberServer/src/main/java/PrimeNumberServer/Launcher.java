c8db61e471737b2f316a58423fe93ebd0bab754d
juliamazz <90276495+juliamazz@users.noreply.github.com>
2023-07-31 14:14:21 -0400

    Delete PrimeNumberServer directory

--- PrimeNumberServer/src/main/java/PrimeNumberServer/Launcher.java
+++
@@ -1,8 +0,0 @@
-package PrimeNumberServer;
-
-public class Launcher {
-
-    public static void main(String[] args) {
-        Server.main(args);
-    }
-}
--- PrimeNumberServer/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module serverfx {
-    requires  javafx.controls;
-    requires  java.sql;
-
-    opens PrimeNumberServer to javafx.fxml;
-    exports PrimeNumberServer;
-}
