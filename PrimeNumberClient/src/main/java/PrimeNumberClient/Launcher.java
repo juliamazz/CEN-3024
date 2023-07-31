5e6279082ff4970ba8aca63bd94bdc49ec8894f7
juliamazz <90276495+juliamazz@users.noreply.github.com>
2023-07-31 14:14:13 -0400

    Delete PrimeNumberClient directory

--- PrimeNumberClient/src/main/java/PrimeNumberClient/Launcher.java
+++
@@ -1,8 +0,0 @@
-package PrimeNumberClient;
-
-public class Launcher {
-
-    public static void main(String[] args) {
-        Client.main(args);
-    }
-}
--- PrimeNumberClient/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module clientfx {
-    requires  javafx.controls;
-    requires  java.sql;
-
-    opens PrimeNumberClient to javafx.fxml;
-    exports PrimeNumberClient;
-}
