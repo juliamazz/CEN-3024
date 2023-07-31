42ae10a3e28bacb9dba42f27321f0a7dc3f4f9bf
juliamazz <90276495+juliamazz@users.noreply.github.com>
2023-07-31 14:27:04 -0400

    Delete WordFrequencyAnalyzer directory

--- WordFrequencyAnalyzer/Documentation/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"com.example.gui","l":"com.example.WordFrequencyAnalyzer"}];updateSearchResults();
--- WordFrequencyAnalyzer/Documentation/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.example.WordFrequencyAnalyzer","l":"App"},{"p":"com.example.WordFrequencyAnalyzer","l":"Controller"},{"p":"com.example.WordFrequencyAnalyzer","l":"Word"},{"p":"com.example.WordFrequencyAnalyzer","l":"WordCounter"}];updateSearchResults();
